a93092ff45819d4310e7f68b0aaf6117a7f9a08a
lexxycool <maxirexy16@yahoo.com>
2026-06-27 14:11:12 -0500

    adding dashboard

---
+++ dashboard/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, jsonify, request
+import os
+
+app = Flask(__name__, static_folder="static")
+
+# stores latest scan from scanner
+latest_devices = []
+
+
+# gets data from local server and upload
+@app.route("/api/upload", methods=["POST"])
+def upload():
+    global latest_devices
+
+    data = request.get_json()
+
+    if not data:
+        return jsonify({
+            "error": "No data received"
+        }), 400
+    
+    latest_devices = data
+
+    return jsonify({
+        "message": "Scan uploaded successfully",
+        "count": len(latest_devices)
+    })
+
+
+# frontend page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# frontend data here
+@app.route("/api/devices", methods=["GET"])
+def devices():
+    return jsonify(latest_devices)
+
+
+@app.route("/api/health")
+def health():
+    return jsonify({
+        "status": "running",
+        "devices_stored": len(latest_devices)
+    })
+
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
--- scanner/scan.py
+++ scanner/scan.py
@@ -1,89 +1,4 @@
-
-# from flask import Flask, render_template, jsonify, request
-# import subprocess, re, socket, uuid
-
-
-# app = Flask(__name__, static_folder="static")
-
-# def run_map(network):
-
-#     # Get the local IP address
-    
-#     my_ip = socket.gethostbyname(socket.gethostname())
-
-#     my_mac = ':'.join(
-#     f'{(uuid.getnode() >> i) & 0xff:02X}'
-#     for i in range(40, -1, -8)
-# )
-
-#     result = subprocess.run(
-#         ["nmap", "-sn", network], 
-#         capture_output=True, 
-#         text=True
-#     )
-
-#     devices = {}
-
-#     current = None
-
-#     for line in result.stdout.splitlines():
-#         if "Nmap scan report for" in line:
-#             if current and "ip" in current:
-#                 devices[current["ip"]] = current
-#             current = {}
-
-#             match = re.search(r"Nmap scan report for (.*?) \((.*?)\)", line)
-#             if match:
-#                 current["host"] = match.group(1)
-#                 current["ip"] = match.group(2)
-                
-#         elif "MAC Address" in line and current:
-#                 current["mac"] = None
-#                 mac = re.search(r"MAC Address: ([A-Fa-f0-9:]+)", line)
-#                 if mac:
-#                     current["mac"] = mac.group(1)
-#                 if current["ip"] == my_ip:
-#                     current["mac"] = my_mac
-#         elif current and "latency" in line:
-#                 latency = re.search(r"\(([^)]+)\s+latency\)", line)
-#                 if latency:
-#                     current["latency"] = latency.group(1)
-        
-#     if current and "ip" in current:
-#         devices[current["ip"]] = current
-
-#     return list(devices.values())
-
-
-# @app.route("/")
-# def home():
-#     return render_template("index.html")
-
-
-# @app.route("/scan")
-# def scan():
-
-#     network = request.args.get(
-#         "network",
-#         "192.168.1.0/24"
-#     )
-#     devices = run_map(network) 
-#     return jsonify(devices)
-
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-
-from flask import Flask, render_template, jsonify, request
-import subprocess
-import re
-import socket
-import uuid
-# import requests
-
-app = Flask(__name__, static_folder="static")
+import requests, re, socket, subprocess, uuid
 
 
 def get_vendor(mac):
@@ -198,23 +113,21 @@ def run_map(network):
     return list(devices.values())
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
 
+if __name__ == "__main__":
+    print("Scanner started....")
 
-@app.route("/scan")
-def scan():
+    network = "192.168.1.0/24"
 
-    network = request.args.get(
-        "network",
-        "192.168.1.0/24"
-    )
+    print("Running nmap....")
 
     devices = run_map(network)
 
-    return jsonify(devices)
+    # post to flask server
+    requests.post(
+        "http://127.0.0.1:8000/api/upload",
+        json=devices
+    )
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
